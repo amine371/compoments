815d310f80bbcd99073d11a9c6ad87e59040db40
amine371 <63712749+amine371@users.noreply.github.com>
2020-05-25 11:01:59 +0100

    Add files via upload

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import fullname from './profile/FullName' ;
+import profilphoto from './profile/ProfilPhoto' ;
+import address from './profile/Address' ;
+
+const profil =() =>{
+return
+<div className ="profilcontainer">
+    <fullname/>
+    <profilphoto/>
+    <address/>
+    </div>
+    );
+
+
+}
+export default profil;
---
+++ src/profil/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const fullname=()=>{
+    return(
+        <div>
+            <div className='fullname'
+            <h1>amine-khadraoui:</h1>
+            
+            </div>
+    );
+}
+export default fullname ;
