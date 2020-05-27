eae9326278db3836bb90bea52f564543f8f61a0b
amine371 <63712749+amine371@users.noreply.github.com>
2020-05-27 14:14:48 +0100

    Add files via upload

---
+++ src/profil/Address.js
@@ -0,0 +1,12 @@
+importer  React à  partir de  'react' ;
+const  Adresse  = ( ) =>  {
+  retour  (
+    < div  className = "address" >
+    < h2 > Contact: < / h2 >
+    < P > < a  href = "tel: 25827757" > Tél: 25827757 < / a > < / p >
+    < P > < a  href = "mb: 25827757" > khadraouiamine3@gmail.com < / a > < / p >
+    < adresse > khadraouiamine3@gmail.com < / adresse >
+    < / div >
+  ) ;
+}
+exporter l'  adresse par défaut  ;p;
---
+++ src/profil/ProfilPhoto.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import photo from '.img/profil.jpg'
+const profilphoto=()=>{
+    return(
+        <div>
+        <img src ={photo} alt = "profilphoto" / >;
+    );
+}
+export default profilphoto ;
