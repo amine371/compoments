4b4c1dcd87bcf72c57e1d3a295b267030f16600f
amine371 <63712749+amine371@users.noreply.github.com>
2020-05-27 14:13:19 +0100

    Add files via upload

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+  
+importer  React à  partir de  'react' ;
+importer  FullName  depuis  './profile/FullName' ;
+importer  ProfilPhoto  depuis  './profile/ProfilPhoto' ;
+importer l'  adresse  depuis  './profile/Address' ;
+import  './Main.css' ;
+ profil  const = ( )  =>  {
+  retour  (
+    < div  className = "profilContainer" >
+    < FullName / >
+    < ProfilPhoto / >
+    < Adresse / >
+    < / div >
+  ) ;
+}
+
+exporter  le  profil par défaut ;
