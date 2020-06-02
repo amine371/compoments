017eff5ef6e4f1595a4e76b1afd74f74c0fc7033
amine371 <63712749+amine371@users.noreply.github.com>
2020-06-02 15:27:47 +0100

    Update Address.js

--- src/profil/Address.js
+++ src/profil/Address.js
@@ -1,7 +1,7 @@
 import  React from  'react' ;
 const  Adresse = ()=>  {
   retour(
-    <div  className = "address" >
+    <div  className = "Adresse" >
     <h2> Contact: < / h2 >
     <P> <a href = "tel: 25827757" > Tél: 25827757 </a> </p>
     <P> <a href = "mb: 25827757" > khadraouiamine3@gmail.com </a> </p>
