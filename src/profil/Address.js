309ed68f3a721ce1c885807525d2fde758f516d4
amine371 <63712749+amine371@users.noreply.github.com>
2020-06-05 12:25:54 +0100

    Update Address.js

--- src/profil/Address.js
+++ src/profil/Address.js
@@ -1,12 +1,19 @@
-import  React from  'react' ;
-const  Adresse = ()=>  {
-  retour(
-    <div  className = "Adresse" >
-    <h2> Contact: < / h2 >
-    <P> <a href = "tel: 25827757" > Tél: 25827757 </a> </p>
-    <P> <a href = "mb: 25827757" > khadraouiamine3@gmail.com </a> </p>
-    < address> khadraouiamine3@gmail.com </address>
-    < / div >
-  ) ;
-}
-export default Address;
+import React from "react";
+
+const Addresse = () =>{
+  return (
+    <div className="Adresse">
+      <h2> Contact: </h2>
+      <p>
+        {" "}
+        <a href="tel: 25827757"> Tél: 25827757 </a>{" "}
+      </p>
+      <p>
+        {" "}
+        <a href="mb: 25827757"> khadraouiamine3@gmail.com </a>{" "}
+      </p>
+      <address> khadraouiamine3@gmail.com </address>
+    </div>
+  );
+};
+export default Addresse;
